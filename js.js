f3ddc34917af9972f6536a38cb88c24fe2262120
DemoK121 <99528400+DemoK121@users.noreply.github.com>
2022-03-20 14:38:58 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,6 @@
+
+// declaration of variables
+
 var teamak = "Andre & Karlien";
 var teamdk = "Demetri & Kelly";
 var teamad = "Andre & Demetrius";
@@ -5,41 +8,166 @@ var teamkk = "Kelly & Karlien";
 var teamka = "Kelly & Andre";
 var teamkd = "Karlien & Demetri"
 
+var activeteam1 = "";
+var activeteam2 = "";
 
-
-
+var randomholder = "";
 // all button 1 clicks
 function clickfunc1ak() {
   console.log(teamak);
 
-$("#team1btn").html(teamak);
+  if (activeteam2 === teamak){
+  alert("Already Selected");
+  }
+  else {
+  activeteam1 = teamak;
+  $("#team1btn").html(activeteam1);
 
-var team1sel = teamak;
 }
+}
+
+function clickfunc1dk() {
+  console.log(teamdk);
+
+  if (activeteam2 === teamdk){
+  alert("Already Selected");
+  }
+  else {
+  activeteam1 = teamdk;
+  $("#team1btn").html(activeteam1);
+}
+}
+
+function clickfunc1ad() {
+  console.log(teamad);
+
+  if (activeteam2 === teamad){
+  alert("Already Selected");
+  }
+  else {
+  activeteam1 = teamad;
+  $("#team1btn").html(activeteam1);
+}
+}
+
+function clickfunc1kk() {
+  console.log(teamkk);
+
+  if (activeteam2 === teamkk){
+  alert("Already Selected");
+  }
+  else {
+  activeteam1 = teamkk;
+  $("#team1btn").html(activeteam1);
+}
+}
+
+function clickfunc1ka() {
+  console.log(teamka);
+
+  if (activeteam2 === teamka){
+  alert("Already Selected");
+  }
+  else {
+  activeteam1 = teamka;
+  $("#team1btn").html(activeteam1);
+}
+}
+
+function clickfunc1kd() {
+  console.log(teamkd);
+
+  if (activeteam2 === teamkd){
+  alert("Already Selected");
+  }
+  else {
+  activeteam1 = teamkd;
+  $("#team1btn").html(activeteam1);
+}
+}
+
+
+
 
 
 
 // all button 2 clicks
 
+
 function clickfunc2ak() {
   console.log(teamak);
 
-if (team1sel = teamak) {
-  console.log("error");
+  if (activeteam1 === teamak){
   alert("Already Selected");
 }
 else {
-$("#team2btn").html(teamak);
+activeteam2 = teamak;
+$("#team2btn").html(activeteam2);
+
 }
+
+
+
 }
 
 function clickfunc2dk() {
   console.log(teamdk);
-
-// if (team1sel == teamak) {
-//
-// }
-// else {
-$("#team2btn").html(teamdk);
-// }
+  if (activeteam1 === teamdk){
+  alert("Already Selected");
 }
+else {
+  activeteam2 = teamdk;
+  $("#team2btn").html(activeteam2);
+}
+}
+
+function clickfunc2ad() {
+  console.log(teamad);
+  if (activeteam1 === teamad){
+  alert("Already Selected");
+}
+else {
+  activeteam2 = teamad;
+  $("#team2btn").html(activeteam2);
+}
+}
+
+function clickfunc2kk() {
+  console.log(teamkk);
+  if (activeteam1 === teamkk){
+  alert("Already Selected");
+}
+else {
+  activeteam2 = teamkk;
+  $("#team2btn").html(activeteam2);
+}
+}
+
+function clickfunc2ka() {
+  console.log(teamka);
+  if (activeteam1 === teamka){
+  alert("Already Selected");
+}
+else {
+  activeteam2 = teamka;
+  $("#team2btn").html(activeteam2);
+}
+}
+
+function clickfunc2kd() {
+  console.log(teamkd);
+  if (activeteam1 === teamkd){
+  alert("Already Selected");
+}
+else {
+  activeteam2 = teamkd;
+  $("#team2btn").html(activeteam2);
+}
+}
+
+
+
+// document.getElementById("2option1").setAttribute("class", "");
+  // var btnelim = document.getElementById("2option1");
+  //
+  // btnelim.onclick = false;
